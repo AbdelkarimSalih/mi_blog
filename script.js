d3c8685d180d806fd2a0b629dc0a64f7ae5670b1
abdelkarim <salih@gmail.com>
2026-01-15 19:27:12 +0100

    Etapa 5: personalización del script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Mensaje que aparece en la consola cuando se carga la página
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Bienvenido al Blog de abdelkarim. La página se ha cargado correctamente.');
+  console.log('¡Bienvenido a mi blog! soy abdelkarim');
+   alert("¡Gracias por visitar mi blog!");
 });
